e7f8351bca3f730e6207df374a2a80220a71f68d
Edwin <edwinclavijo22@gmail.com>
2017-06-14 13:02:07 -0500

    Edite encabezamientos SIAAF

--- index.php
+++ index.php
@@ -68,7 +68,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<h1><a class="navbar-brand" href="index.php">CIAF</a></h1>
+					<h1><a class="navbar-brand" href="index.php">SIAAF</a></h1>
 				</div>
 				<!-- Collect the nav links, forms, and other content for toggling -->
 				<?php
